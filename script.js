206f6c9c2f8050e0fc44fb1ad827c4d8ae842f48
Sudeeeep <sudeepn15@gmail.com>
2022-04-16 01:13:20 +0530

    created grid with user defined size

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const gridContainer = document.createElement("div");
+let gridChild;
+
 gridContainer.classList.add("grid-container");
 document.body.append(gridContainer);
 gridContainer.style.cssText =
     `width: 60%;
     height: 80%;
     margin: 50px auto;
-    display: grid;
-    grid-template-columns: repeat(16, 1fr);`
+    display: grid;`
 
-let gridChild;
-for (let i = 0; i < 256; i++) {
-    gridChild = document.createElement("div");
-    gridChild.classList.add("grid-child");
-    gridContainer.appendChild(gridChild);
-    gridChild.style.cssText =
-        `border: 1px solid black;`
+let n = document.getElementById("size");
+let enterBtn = document.getElementById("submit");
+
+enterBtn.addEventListener("click", () => createGrid(n.value));
+
+function createGrid(n) {
+
+    removeGrid(gridContainer);
+    for (let i = 0; i < n * n; i++) {
+
+        gridContainer.style.gridTemplateColumns = `repeat(${n},1fr)`
+        gridChild = document.createElement("div");
+        gridChild.classList.add("grid-child");
+        gridContainer.appendChild(gridChild);
+        gridChild.style.cssText =
+            `border: 0.5px solid black;`
+    }
+
+}
+
+function removeGrid(node) {
+    while (node.lastElementChild) {
+        node.removeChild(node.lastElementChild);
+    }
 }
