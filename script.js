32648dde9dcdcbbbdaf09ff222b834299d3bfa13
Sudeeeep <sudeepn15@gmail.com>
2022-04-21 00:10:57 +0530

    Added color picker

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const gridContainer = document.createElement("div");
-let gridChild;
+let gridChild, color = "#000000";
 
-gridContainer.classList.add("grid-container");
-document.body.append(gridContainer);
-gridContainer.style.cssText =
-    `width: 60%;
-    height: 80%;
-    margin: 50px auto;
-    display: grid;`
-
-let n = document.getElementById("size");
-let enterBtn = document.getElementById("submit");
+const gridContainer = document.querySelector(".grid-container");
+const n = document.getElementById("size");
+const enterBtn = document.getElementById("submit");
+const colorBtn = document.getElementById("color-btn");
 
 enterBtn.addEventListener("click", () => createGrid(n.value));
+
+colorBtn.addEventListener("input", (e) => {
+    color = e.target.value;
+    return color;
+});
 
 function createGrid(n) {
 
     removeGrid(gridContainer);
+
     for (let i = 0; i < n * n; i++) {
 
         gridContainer.style.gridTemplateColumns = `repeat(${n},1fr)`
+        gridContainer.style.gridTemplateRows = `repeat(${n},1fr)`
         gridChild = document.createElement("div");
         gridChild.classList.add("grid-child");
         gridContainer.appendChild(gridChild);
         gridChild.style.cssText =
             `border: 0.5px solid black;`
+        gridChild.addEventListener("mouseover", colorFill);
     }
 
 }
 
 function removeGrid(node) {
+
     while (node.lastElementChild) {
         node.removeChild(node.lastElementChild);
     }
+}
+
+function colorFill(e) {
+    e.target.style.backgroundColor = color;
 }
